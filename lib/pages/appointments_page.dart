46e7c494d13a1a504e5634d56488b02b349f6c3e
MatthewFante <matthew.fante@gmail.com>
2024-04-30 11:21:06 -0400

    separated appointments_page and appointment_widget into separate classes

--- lib/pages/appointments_page.dart
+++ lib/pages/appointments_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:intl/intl.dart';
 import 'package:untitled/models/meet_and_greet_request.dart';
 import 'package:untitled/models/pet.dart';
+import 'package:untitled/widgets/appointment_widget.dart'; // Import the new file with the widget logic
 
 class AppointmentsPage extends StatefulWidget {
   const AppointmentsPage({Key? key}) : super(key: key);
@@ -42,15 +42,11 @@ class _AppointmentsPageState extends State<AppointmentsPage> {
         future: _getCurrentUserId(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
+            return const Center(child: CircularProgressIndicator());
           }
 
           if (snapshot.hasError) {
-            return const Center(
-              child: Text("Error fetching user ID."),
-            );
+            return const Center(child: Text("Error fetching user ID."));
           }
 
           final userId = snapshot.data!;
@@ -59,139 +55,29 @@ class _AppointmentsPageState extends State<AppointmentsPage> {
             stream: MeetAndGreetRequest.streamRequestsByRequesterId(userId),
             builder: (context, streamSnapshot) {
               if (streamSnapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
+                return const Center(child: CircularProgressIndicator());
               }
 
               if (streamSnapshot.hasError) {
                 return const Center(
-                  child: Text("Error fetching meet & greet requests."),
-                );
+                    child: Text("Error fetching meet & greet requests."));
               }
 
               final requests = streamSnapshot.data ?? [];
 
               if (requests.isEmpty) {
                 return const Center(
-                  child: Text("No meet & greet requests found."),
-                );
+                    child: Text("No meet & greet requests found."));
               }
 
               return ListView.builder(
                 itemCount: requests.length,
                 itemBuilder: (context, index) {
                   final request = requests[index];
-                  final status = request.status;
-
-                  return FutureBuilder<Pet>(
-                    future: _fetchPetDetails(request.petId),
-                    builder: (context, petSnapshot) {
-                      if (petSnapshot.connectionState ==
-                          ConnectionState.waiting) {
-                        return const Card(
-                          child: ListTile(
-                            title: Text("Loading pet details..."),
-                            trailing: CircularProgressIndicator(),
-                          ),
-                        );
-                      }
-
-                      if (petSnapshot.hasError) {
-                        return const Card(
-                          child: ListTile(
-                            title: Text("Error loading pet details"),
-                          ),
-                        );
-                      }
-
-                      final pet = petSnapshot.data;
-                      final petName = pet?.name ?? "Unknown";
-                      final petImageUrl = pet?.imageUrls.isNotEmpty == true
-                          ? pet!.imageUrls[0]
-                          : null;
-
-                      return Card(
-                          elevation: 1, // Add a subtle shadow
-                          child: Row(
-                            children: [
-                              petImageUrl != null
-                                  ? Padding(
-                                      padding: const EdgeInsets.fromLTRB(
-                                          15, 30, 15, 30),
-                                      child: CircleAvatar(
-                                        backgroundImage:
-                                            NetworkImage(petImageUrl),
-                                      ),
-                                    )
-                                  : const Padding(
-                                      padding: EdgeInsets.all(15.0),
-                                      child: CircleAvatar(
-                                        child: Icon(Icons.pets),
-                                      ),
-                                    ),
-                              SizedBox(
-                                width: 250,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text("Meet & Greet w/ $petName",
-                                        style: const TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold)),
-                                    Text(
-                                      "${DateFormat.yMMMd().format(request.meetDate)} @ ${request.meetTime}",
-                                    ),
-                                    status == "approved"
-                                        ? Text(status.toUpperCase(),
-                                            style: const TextStyle(
-                                                color: Colors.green,
-                                                fontWeight: FontWeight.bold))
-                                        : Text(status.toUpperCase(),
-                                            style: const TextStyle(
-                                                color: Colors.red,
-                                                fontWeight: FontWeight.bold))
-                                  ],
-                                ),
-                              ),
-                              ElevatedButton(
-                                onPressed: () async {
-                                  final confirmCancel = await showDialog<bool>(
-                                    context: context,
-                                    builder: (context) => AlertDialog(
-                                      title: const Text(
-                                        "Cancel Request",
-                                      ),
-                                      content: const Text(
-                                          "Are you sure you want to cancel this request?"),
-                                      actions: [
-                                        TextButton(
-                                          onPressed: () {
-                                            Navigator.pop(context, false);
-                                          },
-                                          child: const Text("No"),
-                                        ),
-                                        ElevatedButton(
-                                          onPressed: () {
-                                            Navigator.pop(context, true);
-                                          },
-                                          child: const Text("Yes"),
-                                        ),
-                                      ],
-                                    ),
-                                  );
-
-                                  if (confirmCancel == true) {
-                                    await _cancelRequest(request.requestId);
-                                  }
-                                },
-                                child: const Text("Cancel",
-                                    style: TextStyle(color: Color(0xff990000))),
-                              ),
-                            ],
-                          ));
-                      //;
-                    },
+                  return AppointmentWidget(
+                    request: request,
+                    cancelRequest: _cancelRequest,
+                    fetchPetDetails: _fetchPetDetails,
                   );
                 },
               );
@@ -205,11 +91,9 @@ class _AppointmentsPageState extends State<AppointmentsPage> {
   Future<Pet> _fetchPetDetails(String petId) async {
     final petDoc =
         await FirebaseFirestore.instance.collection('pets').doc(petId).get();
-
     if (!petDoc.exists) {
       throw Exception("Pet not found");
     }
-
     return Pet.fromJson(petDoc.data()!);
   }
 }
---
+++ lib/widgets/appointment_widget.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:untitled/models/meet_and_greet_request.dart';
+import 'package:untitled/models/pet.dart';
+
+class AppointmentWidget extends StatelessWidget {
+  final MeetAndGreetRequest request;
+  final Future<Pet> Function(String) fetchPetDetails;
+  final Future<void> Function(String) cancelRequest;
+
+  const AppointmentWidget({
+    required this.request,
+    required this.fetchPetDetails,
+    required this.cancelRequest,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Pet>(
+      future: fetchPetDetails(request.petId),
+      builder: (context, petSnapshot) {
+        if (petSnapshot.connectionState == ConnectionState.waiting) {
+          return const Card(
+            child: ListTile(
+              title: Text("Loading pet details..."),
+              trailing: CircularProgressIndicator(),
+            ),
+          );
+        }
+
+        if (petSnapshot.hasError) {
+          return const Card(
+            child: ListTile(
+              title: Text("Error loading pet details"),
+            ),
+          );
+        }
+
+        final pet = petSnapshot.data;
+        final petName = pet?.name ?? "Unknown";
+        final petImageUrl =
+            pet?.imageUrls.isNotEmpty == true ? pet!.imageUrls[0] : null;
+
+        return Card(
+          elevation: 1, // Add a subtle shadow
+          child: Row(
+            children: [
+              petImageUrl != null
+                  ? Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 30, 15, 30),
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(petImageUrl),
+                      ),
+                    )
+                  : const Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: CircleAvatar(
+                        child: Icon(Icons.pets),
+                      ),
+                    ),
+              SizedBox(
+                width: 250,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Meet & Greet w/ $petName",
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    Text(
+                        "${DateFormat.yMMMd().format(request.meetDate)} @ ${request.meetTime}"),
+                    request.status == "approved"
+                        ? Text(request.status.toUpperCase(),
+                            style: const TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold))
+                        : Text(request.status.toUpperCase(),
+                            style: const TextStyle(
+                                color: Colors.red,
+                                fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final confirmCancel = await showDialog<bool>(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text("Cancel Request"),
+                      content: const Text(
+                          "Are you sure you want to cancel this request?"),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context, false);
+                          },
+                          child: const Text("No"),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context, true);
+                          },
+                          child: const Text("Yes"),
+                        ),
+                      ],
+                    ),
+                  );
+
+                  if (confirmCancel == true) {
+                    await cancelRequest(request.requestId);
+                  }
+                },
+                child: const Text("Cancel",
+                    style: TextStyle(color: Color(0xff990000))),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
